f69417627806c2699a49bf396b1061758af36325
mheinemann28 <heinemam@oregonstate.edu>
2018-03-13 12:31:00 -0400

    Added partition test

--- projects/heinemam/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/heinemam/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,8 @@
 
 
 import junit.framework.TestCase;
+import java.util.Random;
+import java.io.*;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -12,24 +14,64 @@ import junit.framework.TestCase;
 
 public class UrlValidatorTest extends TestCase {
 
-
+	String[] validSchemes = {"http://", "ftp://", "h3t://", ""};
+	String[] invalidSchemes = {"3ht://", "http:/", "http:", "http/", "://"};
+	
+	String[] validAuthority = {"www.google.com", "google.com", "www.amazon.com", "0.0.0.0", "255.255.255.255"};
+	String[] invalidAuthority = {"go.a", "256.256.256.256", "1.2.3.4", "google"};
+	
+	String[] validPort = {":80", ":0", ""};
+	String[] invalidPort = {":-1", ":65a", ":65636"};
+	
+	String[] validPath = {"/test", "/test/", "/$23", "/test/test"};
+	String[] invalidPath = {"/..", "/test//test", "/../"};
+	
+	
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
    
    
-   public void testManualTest()
+   public void testManualTest() throws IOException
    {
-//You can use this function to implement your manual testing	   
-	   
+	   //You can use this function to implement your manual testing	   
+	   //Code suggested by "Java A Beginner's Guid, 6th Edition" by Schildt, Herbert, 2014
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String url;
+	   System.out.println("Enter URLs for testing, one per line. Type 'stop' on a line by itself to quit");
+	   do {
+		   url = br.readLine();
+		   System.out.println(urlVal.isValid(url));
+	      } while(!url.equals("stop"));
    }
    
-   
+   // test different schemes
    public void testYourFirstPartition()
    {
 	 //You can use this function to implement your First Partition testing	   
-
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String url;
+	   int randAuth, randPort, randPath;
+	   boolean valid;
+	   
+	   Random rand = new Random();
+	   
+	   for(int i = 0; i < validSchemes.length; i++) {
+		   randAuth = rand.nextInt(5);
+		   randPort = rand.nextInt(3);
+		   randPath = rand.nextInt(4);
+		   
+		   url = validSchemes[i] + validAuthority[randAuth] + validPort[randPort] + validPath[randPath];
+		   
+		   System.out.println("Testing URL: " + url);
+		   
+		   valid = urlVal.isValid(url);
+		   
+		   System.out.println("Expected Result: true; Actual Result: " + valid);
+	   }
+	   
    }
    
    public void testYourSecondPartition(){
@@ -44,6 +86,11 @@ public class UrlValidatorTest extends TestCase {
 
    }
    
-
+   public static void main(String args[]) throws IOException
+   {
+	   UrlValidatorTest fct = new UrlValidatorTest("url test");
+	   fct.testManualTest();
+	   fct.testYourFirstPartition();
+   }
 
 }
