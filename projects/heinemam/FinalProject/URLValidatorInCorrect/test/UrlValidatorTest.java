4193d4acc841631bd5f7552b0b72c7fa2c2563ed
mheinemann28 <heinemam@oregonstate.edu>
2018-03-16 18:12:17 -0400

    added partitions

--- projects/heinemam/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/heinemam/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -43,8 +43,13 @@ public class UrlValidatorTest extends TestCase {
 	   System.out.println("Enter URLs for testing, one per line. Type 'stop' on a line by itself to quit");
 	   do {
 		   url = br.readLine();
-		   System.out.println(urlVal.isValid(url));
-	      } while(!url.equals("stop"));
+		   try {
+			   System.out.println(urlVal.isValid(url));
+		   }
+		   catch(Throwable t) {
+			   t.printStackTrace();
+		   }
+	   } while(!url.equals("stop"));
    }
    
    // test different schemes
@@ -53,44 +58,136 @@ public class UrlValidatorTest extends TestCase {
 	 //You can use this function to implement your First Partition testing	   
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   String url;
-	   int randAuth, randPort, randPath;
-	   boolean valid;
-	   
-	   Random rand = new Random();
 	   
 	   for(int i = 0; i < validSchemes.length; i++) {
-		   randAuth = rand.nextInt(5);
-		   randPort = rand.nextInt(3);
-		   randPath = rand.nextInt(4);
 		   
-		   url = validSchemes[i] + validAuthority[randAuth] + validPort[randPort] + validPath[randPath];
+		   url = validSchemes[i] + "www.amazon.com";
 		   
-		   System.out.println("Testing URL: " + url);
+		   System.out.println("\nTesting Valid scheme: " + url);
 		   
-		   valid = urlVal.isValid(url);
-		   
-		   System.out.println("Expected Result: true; Actual Result: " + valid);
+		   try {
+			   System.out.println("Expected Result: true; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
 	   }
 	   
 	   for(int i = 0; i < invalidSchemes.length; i++) {
-		   randAuth = rand.nextInt(5);
-		   randPort = rand.nextInt(3);
-		   randPath = rand.nextInt(4);
 		   
-		   url = invalidSchemes[i] + validAuthority[randAuth] + validPort[randPort] + validPath[randPath];
+		   url = invalidSchemes[i] + "www.amazon.com";
 		   
-		   System.out.println("Testing URL: " + url);
-		   
-		   valid = urlVal.isValid(url);
-		   
-		   System.out.println("Expected Result: false; Actual Result: " + valid);
+		   System.out.println("\nTesting Invalid scheme: " + url);
+		   try {
+			   System.out.println("Expected Result: false; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
 	   }
    }
    
+   // test different authority
    public void testYourSecondPartition(){
 		 //You can use this function to implement your Second Partition testing	   
-
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String url;
+	   
+	   for(int i = 0; i < validAuthority.length; i++) {
+		   
+		   url = "http://" + validAuthority[i];
+		   
+		   System.out.println("\nTesting Valid authority: " + url);
+		   
+		   try {
+			   System.out.println("Expected Result: true; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
+	   }
+	   
+	   for(int i = 0; i < invalidAuthority.length; i++) {
+		   
+		   url = "http://" + invalidAuthority[i];
+		   
+		   System.out.println("\nTesting Invalid authority: " + url);
+		   
+		   try {
+			   System.out.println("Expected Result: false; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
+	   }
    }
+   
+   public void testYourThirdPartition() {
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String url;
+	   
+	   for(int i = 0; i < validPort.length; i++) {
+		   
+		   url = "http://www.amazon.com" + validPort[i];
+		   
+		   System.out.println("\nTesting Valid port: " + url);
+		   
+		   try {
+			   System.out.println("Expected Result: true; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
+	   }
+	   
+	   for(int i = 0; i < invalidPort.length; i++) {
+		   
+		   url = "http://www.amazon.com" + invalidPort[i];
+		   
+		   System.out.println("\nTesting Invalid port: " + url);
+		   
+		   try {
+			   System.out.println("Expected Result: false; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
+	   }
+   }
+   
+   public void testYourFourthPartition() {
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String url;
+	   
+	   for(int i = 0; i < validPath.length; i++) {
+		   
+		   url = "http://www.amazon.com" + validPath[i];
+		   
+		   System.out.println("\nTesting Valid path: " + url);
+		   
+		   try {
+			   System.out.println("Expected Result: true; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
+	   }
+	   
+	   for(int i = 0; i < invalidPath.length; i++) {
+		   
+		   url = "http://www.amazon.com" + invalidPath[i];
+		   
+		   System.out.println("\nTesting Invalid path: " + url);
+		   
+		   try {
+			   System.out.println("Expected Result: false; Actual Result: " + urlVal.isValid(url));
+		   }
+		   catch(Throwable t){
+			   t.printStackTrace();
+		   }
+	   }
+   }
+   
    //You need to create more test cases for your Partitions if you need to 
    
    public void testIsValid()
@@ -104,6 +201,9 @@ public class UrlValidatorTest extends TestCase {
 	   UrlValidatorTest fct = new UrlValidatorTest("url test");
 	   fct.testManualTest();
 	   fct.testYourFirstPartition();
+	   fct.testYourSecondPartition();
+	   fct.testYourThirdPartition();
+	   fct.testYourFourthPartition();
    }
 
 }
